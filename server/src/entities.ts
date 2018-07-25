1850776139c4606c4a4c5869ff9b5d3c1ccdeb88
Maximilian Täschner <maximilian.taeschner@gmail.com>
2018-07-25 21:50:13 +0200

    docs: adds docstring to entities

--- server/src/entities.ts
+++ server/src/entities.ts
@@ -2,6 +2,10 @@ import { AccessToken } from './auth/AccessToken';
 import { ResetPasswordToken } from './auth/ResetPasswordToken';
 import { User } from './user/User';
 
+/**
+ * All entities registered with TypeORM.
+ * Simplifies creating connections in test and production.
+ */
 export function getEntities() {
   return [
     User,
