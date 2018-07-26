878f26474b5d4200324eff2da87ed6d079dbba0f
Maximilian Täschner <maximilian.taeschner@gmail.com>
2018-07-26 23:49:53 +0200

    tests: extracts PasswordReset tests

---
+++ server/src/auth/__tests__/PasswordReset.test.ts
@@ -0,0 +1,75 @@
+import { Connection } from 'typeorm';
+import { Container } from '../../../node_modules/typedi';
+import { OAuth } from '../OAuth';
+import { PasswordReset } from '../PasswordReset';
+import { PasswordResetToken } from '../PasswordResetToken';
+import { User } from '../../user/User';
+import { createTestConnection } from '../../__tests__/createTestConnection';
+
+describe(PasswordReset.name, () => {
+  let connection: Connection;
+
+  beforeAll(async () => {
+    process.env.PASSWORDS_PEPPER = 'my_global_pepper';
+    connection = await createTestConnection();
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('create and accept a password reset token', async () => {
+    const userRepo = connection.getRepository(User);
+    const passwordReset = Container.get(PasswordReset);
+    const oauth = Container.get(OAuth);
+
+    const email = 'email+OAuthCreateAcceptPasswordResetToken@example.com';
+    const password = 'my_super_secure_password';
+    const newPassword = 'my_super_secure_password_new';
+    const user = await userRepo.save(userRepo.create({ email, password }));
+    const resetTokenResponse = await passwordReset.getToken(email);
+    expect(resetTokenResponse!.validUntil).toBeGreaterThan(Date.now());
+
+    const tokenRepo = connection.getRepository(PasswordResetToken);
+    const decryptedToken = PasswordResetToken.decrypt(resetTokenResponse!.token);
+    const existsInDatabase = await tokenRepo.findOne({ token: decryptedToken });
+    expect(existsInDatabase).toBeTruthy();
+    expect(existsInDatabase!.user).toBeTruthy();
+
+    const resetResult = await passwordReset.resetPassword(resetTokenResponse!.token, newPassword);
+    expect(resetResult).toBeTruthy();
+
+    const existInDatabaseAfter = await tokenRepo.findOne({ token: decryptedToken });
+    expect(existInDatabaseAfter).toBeFalsy();
+
+    const passwordIsCorrect = await oauth
+    .createUserCredentialsAccessToken({ email, password: newPassword });
+    expect(passwordIsCorrect).toBeTruthy();
+  });
+
+  it('does not accept an expired token', async () => {
+    const userRepo = connection.getRepository(User);
+    const passwordReset = Container.get(PasswordReset);
+
+    const email = 'email+OAuthNotAcceptExpiredPasswordResetToken@example.com';
+    const password = 'my_super_secure_password';
+    const newPassword = 'my_super_secure_password_new';
+    const user = await userRepo.save(userRepo.create({ email, password }));
+    const resetToken = await passwordReset.getToken(email, -1000);
+    const resetResult = await passwordReset.resetPassword(resetToken!.token, newPassword);
+    expect(resetResult).toBeFalsy();
+  });
+
+  it('does not accept an non existing token', async () => {
+    const userRepo = connection.getRepository(User);
+    const passwordReset = Container.get(PasswordReset);
+    const email = 'email+OAuthNotAcceptInvalidPasswordResetToken@example.com';
+    const password = 'my_super_secure_password';
+    const newPassword = 'my_super_secure_password_new';
+    const user = await userRepo.save(userRepo.create({ email, password }));
+    const resetResult = await passwordReset.resetPassword(
+      PasswordResetToken.encrypt('DOES_NOT_EXIST'),
+      newPassword);
+    expect(resetResult).toBeFalsy();
+  });
+});
