aaa17957f1d19ab7b9fa1f9cb232656c26117fcf
Maximilian Täschner <maximilian.taeschner@gmail.com>
2018-08-30 15:54:49 +0200

    refactor: improve styling for Notification

--- client/src/notification/Notification.tsx
+++ client/src/notification/Notification.tsx
@@ -2,10 +2,17 @@ import './Notification.scss';
 
 import * as React from 'react';
 
+import { FaCheckCircle, FaExclamationTriangle } from 'react-icons/fa';
+
 import { classnames } from '@hydrokit/utils';
 import posed from 'react-pose';
 
 export type NotificationType = 'error' | 'success';
+
+const iconForType = {
+  'error': FaExclamationTriangle,
+  'success': FaCheckCircle,
+};
 
 export interface NotificationProps {
   type: NotificationType;
@@ -18,9 +25,15 @@ export const Notification: React.StatelessComponent<NotificationProps> = props =
     'notification--' + props.type,
     props.className
   );
+  const Icon = iconForType[props.type];
   
   return props.children ? (
-    <div className={className}>{props.children}</div>
+    <div className={className}>
+      <div className="notification__icon"><Icon /></div>
+      <div className="notification__message">
+        {props.children}
+      </div>
+    </div>
   ) : null;
 };
 
